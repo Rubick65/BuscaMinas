aaff6d13157c6ac9027535a5db34867dc7048806
marti <martinandraderuben@gmail.com>
2025-06-05 17:16:43 +0200

    Busca Minas 0.0

---
+++ src/InterfazBuscaMinas.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class InterfazBuscaMinas {
+
+    public InterfazBuscaMinas() {
+        inizializar();
+    }
+
+    private void inizializar() {
+        crearVentanaBuscaMinas();
+
+    }
+
+    private void crearVentanaBuscaMinas() {
+        JFrame buscaMinas = new JFrame("Busca Minas");
+        buscaMinas.setSize(new Dimension(500, 400));
+        buscaMinas.setResizable(false);
+        buscaMinas.setVisible(true);
+    }
+}
